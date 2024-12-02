c542b48d6576f0e20e94d119cb238b7c0173e711
Ronit-11 <98330660+Ronit-11@users.noreply.github.com>
2024-12-02 20:49:04 +0300

    Github Actions added and modified UI (#1)

--- lib/screens/property_details_screen.dart
+++ lib/screens/property_details_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_endpoint_integration_with_github_actions/providers/property_provider.dart';
-import 'package:flutter_endpoint_integration_with_github_actions/models/property_model.dart';
 import 'package:provider/provider.dart';
 
 class PropertyDetailScreen extends StatelessWidget {
@@ -12,12 +11,133 @@ class PropertyDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final propertyProvider = Provider.of<PropertyProvider>(context);
     final property = propertyProvider.properties.firstWhere((property) => property.id == propertyId);
-    
+
     return Scaffold(
-      appBar: AppBar(title: Text(property.name)),
+      appBar: AppBar(
+        title: Text(property.name),
+        backgroundColor: Color.fromARGB(255, 255, 203, 135),
+      ),
       body: propertyProvider.isLoading
           ? const Center(child: CircularProgressIndicator())
-          : Text(property.propertyLanguages.join('\n')),
+          : Container(
+            color: Color.fromARGB(224, 252, 255, 232),
+            child: ListView(
+              padding: const EdgeInsets.all(16),
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: property.propertyImages.isNotEmpty
+                      ? Image.network(
+                          property.propertyImages[0],
+                          width: double.infinity,
+                          height: 200,
+                          fit: BoxFit.cover,
+                        )
+                      : Container(
+                          width: double.infinity,
+                          height: 200,
+                          color: Colors.orange[200],
+                          child: const Icon(Icons.image, size: 50, color: Colors.white),
+                        ),
+                ),
+                const SizedBox(height: 6),
+                Text(
+                  '${property.address.street}, ${property.address.town}, ${property.address.country}',
+                  style: TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+
+                // Property Description
+                Text(
+                  property.description,
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 2),
+
+                // Property Amenities
+                ExpansionTile(
+                  title: Text('Amenities', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  children: property.propertyAmenities
+                    .where((amenity) => amenity.name.isNotEmpty)
+                    .map((amenity) {
+                    return ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                      title: Text('${amenity.name}'),
+                    );
+                  }).toList(),
+                ),
+
+                // Property Policies
+                ExpansionTile(
+                  title: Text(
+                    'Policies',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  children: property.propertyPolicies
+                      .where((policy) => policy.description.isNotEmpty)
+                      .map((policy) {
+                    return ListTile(
+                      contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                      title: Text(policy.description),
+                    );
+                  }).toList(),
+                ),
+
+
+                // Meal Plan Option
+                if (property.mealOptions.isNotEmpty)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(),
+                    child: Text(
+                      'Meal Plan: ${property.mealOptions.first.plan}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+
+                // Property Languages
+                const SizedBox(height: 16),
+                Text(
+                  'Languages Spoken:',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                for (var language in property.propertyLanguages)
+                  Text(
+                    '- ${language}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                const SizedBox(height: 16),
+
+                // Staff Images (Circular Avatars)
+                if (property.staffImages.isNotEmpty)
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 16),
+                      Text(
+                        'Meet Our Staff:',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 8),
+                      SizedBox(
+                        height: 100,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: property.staffImages.length,
+                          itemBuilder: (ctx, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: CircleAvatar(
+                                radius: 40,
+                                backgroundImage: NetworkImage(property.staffImages[index]),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+              ],
+            ),
+          ),   
     );
   }
 }
--- lib/screens/property_list_screen.dart
+++ lib/screens/property_list_screen.dart
@@ -14,14 +14,10 @@ class _PropertyListScreenState extends State<PropertyListScreen> {
   @override
   void initState() {
     super.initState();
-    // Fetch properties only once when the widget is initialized
+    // Fetch properties once during initialization
     WidgetsBinding.instance.addPostFrameCallback((_) {
       final propertyProvider = Provider.of<PropertyProvider>(context, listen: false);
-      propertyProvider.fetchProperties().then((_) {
-        print('All Properties data in property_list_screen: print');
-        print(propertyProvider.properties);
-        debugPrint('All Properties data in property_list_screen: debugprint');
-      });
+      propertyProvider.fetchProperties();
     });
   }
 
@@ -30,71 +26,106 @@ class _PropertyListScreenState extends State<PropertyListScreen> {
     final propertyProvider = Provider.of<PropertyProvider>(context);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Properties List')),
+      appBar: AppBar(
+        title: const Text('Properties List'),
+        backgroundColor: Color.fromARGB(255, 255, 203, 135),
+      ),
       body: propertyProvider.isLoading
           ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: propertyProvider.properties.length,
-              itemBuilder: (ctx, index) {
-                final property = propertyProvider.properties[index];
-                return ListTile(
-                  title:  Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                property.name,
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
+          : Container(
+              color: Color.fromARGB(224, 252, 255, 232),
+              child: ListView.builder(
+                itemCount: propertyProvider.properties.length,
+                itemBuilder: (ctx, index) {
+                  final property = propertyProvider.properties[index];
 
-                              Text(
-                                'Address: ${property.address.street}, ${property.address.town}, ${property.address.country}',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.grey, 
-                                ),
-                              ),
-                            ],
-                          ),
-
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      if (property.propertyImages.isNotEmpty)
-                        Container(
-                          width: double.infinity,
-                          height: 200,
-                          child: Image.network(
-                            property.propertyImages[0].url,
-                            fit: BoxFit.cover,
-                          ),
+                  return InkWell(
+                    onTap: () async {
+                      // Fetch property details and navigate to the property detail screen
+                      await propertyProvider.fetchPropertyDetails(property.id.toString(), index);
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (context, animation, secondaryAnimation) => PropertyDetailScreen(propertyId: property.id),
+                          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                            const begin = Offset(1.0, 0.0);
+                            const end = Offset.zero;
+                            const curve = Curves.easeInOut;
+                            var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                            var offsetAnimation = animation.drive(tween);
+                            return SlideTransition(position: offsetAnimation, child: child);
+                          },
                         ),
-                      SizedBox(height: 10),
-                      Text(
-                        property.description,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
+                      );
+                    },
+                    child: Card(
+                      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      elevation: 8,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
                       ),
-                    ],
-                  ),
-                  onTap: () async {
-                    final propertyProvider = Provider.of<PropertyProvider>(context, listen: false);
-                    final propertyId = property.id;
-                    // Fetch the property details and update its contents
-                    await propertyProvider.fetchPropertyDetails(propertyId.toString(), index);
-                    
-                    // Navigate to the PropertyDetailScreen
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PropertyDetailScreen(propertyId: property.id),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(15),
+                              topRight: Radius.circular(15),
+                            ),
+                            child: property.propertyImages.isNotEmpty
+                                ? Image.network(
+                                    property.propertyImages[0],
+                                    width: double.infinity,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                  )
+                                : Container(
+                                    width: double.infinity,
+                                    height: 200,
+                                    color: Colors.orange[200],
+                                    child: const Icon(Icons.image, size: 50, color: Colors.white),
+                                  ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  property.name,
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.orangeAccent,
+                                  ),
+                                ),
+
+                                Text(
+                                  '${property.address.street}, ${property.address.town}, ${property.address.country}',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.grey[700],
+                                  ),
+                                ),
+                                
+                                Text(
+                                  property.description,
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.grey[800],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    );
-                  },
-                );
-              },
+                    ),
+                  );
+                },
+              ),
             ),
     );
   }
